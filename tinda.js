91499a705606787c3c61ae80ed1728e5ccaff4cd
kurmananda <edu.kurmananda@gmail.com>
2024-10-08 22:15:10 +0530

    Add files via upload

---
+++ tinda.js
@@ -0,0 +1,59 @@
+function showSidebar()
+{
+    const side = document.querySelector('.sidee')
+    side.style.display = "flex"
+}
+function hideSidebar()
+{
+    const side = document.querySelector('.sidee')
+    side.style.display = "none"
+}
+
+function calcu()
+{
+    const mech = document.getElementById('calcu-matter')
+    console.log(mech.style.display);
+    
+    if(mech.style.display == 'flex'){mech.style.display = 'none'}
+    else{mech.style.display = "flex"}
+}
+function mech()
+{
+    const mech = document.getElementById('mech-matter')
+    console.log(mech.style.display);
+    
+    if(mech.style.display == 'flex'){mech.style.display = 'none'}
+    else{mech.style.display = "flex"}
+}
+function bee()
+{
+    const mech = document.getElementById('bee-matter')
+    console.log(mech.style.display);
+    
+    if(mech.style.display == 'flex'){mech.style.display = 'none'}
+    else{mech.style.display = "flex"}
+}
+function envi()
+{
+    const mech = document.getElementById('envi-matter')
+    console.log(mech.style.display);
+    
+    if(mech.style.display == 'flex'){mech.style.display = 'none'}
+    else{mech.style.display = "flex"}
+}
+function iss()
+{
+    const mech = document.getElementById('iss-matter')
+    console.log(mech.style.display);
+    
+    if(mech.style.display == 'flex'){mech.style.display = 'none'}
+    else{mech.style.display = "flex"}
+}
+function commu()
+{
+    const mech = document.getElementById('commu-matter')
+    console.log(mech.style.display);
+    
+    if(mech.style.display == 'flex'){mech.style.display = 'none'}
+    else{mech.style.display = "flex"}
+}
